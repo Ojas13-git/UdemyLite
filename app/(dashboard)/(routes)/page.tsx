5b0d1101014d088d1d88ce3bfa7b74c86598ddb5
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2023-12-21 19:33:11 +0530

    clerk auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,15 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <div>
-      <p className="text-3xl font-medium text-sky-700">
-        Hello World
-      </p>
-      <Button variant="destructive">
-        Click me
-      </Button>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
